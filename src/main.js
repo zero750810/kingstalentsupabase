b766d3240ae8944bd9a215fac93d6fd997c90acf
zero <zero0810@gmail.com>
2024-03-21 18:42:14 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 // src/main.js
-import { createApp } from '/vue';
+import { createApp } from './vue';
 import App from './App.vue';
 import router from './router';
 import store from './store'; // 引入 Vuex store
 // 在 src/main.js 中
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '/bootstrap/dist/css/bootstrap.min.css';
 // 在 src/main.js 中
-import 'bootstrap';
-import { Modal } from 'bootstrap';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import '/bootstrap';
+import '/bootstrap-icons/font/bootstrap-icons.css';
 
 
 createApp(App)

249d50f38656f7e572c5ed88a825aec2825225e1
zero <zero0810@gmail.com>
2024-03-21 19:05:22 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store'; // 引入 Vuex store
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './node_modules/bootstrap/dist/js/bootstrap';
+import 'bootstrap';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
 
